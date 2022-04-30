33439946fd9750820c2f7d0ed801164b79dcda7d
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-04-30 02:41:53 -0300

    HeroList useMemo

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
 import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
   
-  const heroes = getHeroesByPublisher ( publisher );
+  const heroes = useMemo(() => getHeroesByPublisher ( publisher ), [ publisher ]) ;
   return (
-    <div className='card-columns'>
+    
+    <div className="row justify-content-center animate__animated animate__fadeIn" data-masonry='{"percentPosition": true }' >
       {
         heroes.map ( hero => (
           <HeroCard 
-            key={hero.id} 
-            { ...hero }
+          key={hero.id} 
+          { ...hero }
           />
-        ))
-      }
+          ))
+        }
     </div>
+    
   )
 }
