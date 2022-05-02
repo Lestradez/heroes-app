e8fc53a060cf243b59ac7b6cda79991c3caf513a
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-05-02 20:52:39 -0300

    Public and Private Routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   Routes,
   Route,
 } from "react-router-dom";
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
+  
   return (
     
       <Routes>
-          <Route exact path='/login' element={ <LoginScreen /> } />
-          <Route path='*' element={ <DashboardRoutes /> } />
+          <Route exact path='/login' element={ 
+            <PublicRoute>
+              <LoginScreen /> 
+            </PublicRoute>
+          } />
+          <Route path='*' element= {
+            <PrivateRoute >
+              <DashboardRoutes />  
+            </PrivateRoute>
+            }>
+          </Route>      
+          
       </Routes>
     
   )
---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react'
+import { Navigate } from 'react-router-dom';
+
+import { AuthContext } from '../auth/AuthContext';
+
+export const PublicRoute = ({children}) => {
+
+  const {user} = useContext(AuthContext);
+  
+  return user.logged
+    ? <Navigate to= '/' /> 
+    : children
+ 
+};
+
